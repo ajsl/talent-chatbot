fdf3ca1a3e085b67560122db22035bab4b655da5
jleith <jleith@aquent.com>
2023-03-09 15:31:59 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatbotContainerComponent } from './chatbot-container/chatbot-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChatbotContainerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/chatbot-container/chatbot-container.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatbot-container',
+  templateUrl: './chatbot-container.component.html',
+  styleUrls: ['./chatbot-container.component.scss']
+})
+export class ChatbotContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
